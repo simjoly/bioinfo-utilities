e86b2fefedf6115929f98d8fa3b7bd29056d6aa7
Simon Joly <joly.simon@gmail.com>
2024-07-11 13:41:23 -0400

    Update remove_pcr_dup.py
    Use both reads
    Add progress bar
    Some speed optimization

--- ipyrad/remove_pcr_dup.py
+++ ipyrad/remove_pcr_dup.py
@@ -22,40 +22,49 @@
 from Bio import SeqIO
 import argparse
 import gzip
+from tqdm import tqdm
 
 def Main():
-    Unique_seqs = set()
+    Unique_seqs = {}
     args = ParseArg()
     duplicates = 0
     total = 0
-    with gzip.open("nodup_"+args.read1, "wt") as outfile1:
-        with gzip.open("nodup_"+args.read2, "wt") as outfile2:
-            if args.index == "":
-                with gzip.open(args.read1, "rt") as read1, gzip.open(args.read2, "rt") as read2:
-                    fastq_iter1 = SeqIO.parse(read1, "fastq")
-                    fastq_iter2 = SeqIO.parse(read2, "fastq")
-                    for rec1, rec2 in zip(fastq_iter1, fastq_iter2):
-                        if str(rec1.seq) not in Unique_seqs:
-                            SeqIO.write(rec1, outfile1, "fastq")
-                            SeqIO.write(rec2, outfile2, "fastq")
-                            Unique_seqs.add(str(rec1.seq))
-                        else:
-                            duplicates+=1
-                        total+=1
-            else:
-                with gzip.open(args.read1, "rt") as read1, gzip.open(args.read2, "rt") as read2, gzip.open(args.index, "rt") as index:
-                    fastq_iter1 = SeqIO.parse(read1, "fastq")
-                    fastq_iter2 = SeqIO.parse(read2, "fastq")
-                    fastq_iter3 = SeqIO.parse(index, "fastq")
-                    for rec1, rec2, ind1 in zip(fastq_iter1, fastq_iter2, fastq_iter3):
-                        if (str(ind1.seq) + str(rec1.seq)) not in Unique_seqs:
-                            SeqIO.write(rec1, outfile1, "fastq")
-                            SeqIO.write(rec2, outfile2, "fastq")
-                            Unique_seqs.add(str(ind1.seq) + str(rec1.seq))
-                        else:
-                            duplicates+=1
-                        total+=1
-    print(total," sequences in file\n",duplicates," duplicates (",round(duplicates/total*100,2),"%)")
+    if args.number_of_seqs == 0:
+        print(f"Calculating the number of sequences...")
+        args.number_of_seqs = len(list(SeqIO.parse(gzip.open(args.read1, "rt"), "fastq")))
+    with gzip.open("nodup_" + args.read1, "wt") as outfile1, gzip.open("nodup_" + args.read2, "wt") as outfile2:
+        if args.index == "":
+            with gzip.open(args.read1, "rt") as read1, gzip.open(args.read2, "rt") as read2:
+                fastq_iter1 = SeqIO.parse(read1, "fastq")
+                fastq_iter2 = SeqIO.parse(read2, "fastq")
+                zip_iter = tqdm(zip(fastq_iter1, fastq_iter2), total=args.number_of_seqs)
+                for rec1, rec2 in zip_iter:
+                    seq_key = (rec1.seq, rec2.seq)
+                    if seq_key not in Unique_seqs:
+                        SeqIO.write(rec1, outfile1, "fastq")
+                        SeqIO.write(rec2, outfile2, "fastq")
+                        Unique_seqs[seq_key] = 1
+                    else:
+                        duplicates += 1
+                    total += 1
+                    zip_iter.set_postfix_str(f"Percent Duplicates: {round(duplicates/total*100, 2)}")
+        else:
+            with gzip.open(args.read1, "rt") as read1, gzip.open(args.read2, "rt") as read2, gzip.open(args.index, "rt") as index:
+                fastq_iter1 = SeqIO.parse(read1, "fastq")
+                fastq_iter2 = SeqIO.parse(read2, "fastq")
+                fastq_iter3 = SeqIO.parse(index, "fastq")
+                zip_iter = tqdm(zip(fastq_iter1, fastq_iter2, fastq_iter3), total=args.number_of_seqs)
+                for rec1, rec2, ind1 in zip_iter:
+                    seq_key = (ind1.seq, rec1.seq, rec2.seq)
+                    if seq_key not in Unique_seqs:
+                        SeqIO.write(rec1, outfile1, "fastq")
+                        SeqIO.write(rec2, outfile2, "fastq")
+                        Unique_seqs[seq_key] = 1
+                    else:
+                        duplicates += 1
+                    total += 1
+                    zip_iter.set_postfix_str(f"Percent Duplicates: {round(duplicates/total*100, 2)}")
+    print(f"{total} sequences in file\n{duplicates} duplicates ({round(duplicates/total*100, 2)}%)")
 
 def ParseArg():
     parser = argparse.ArgumentParser(description="Remove pairs of reads that are identical. If "
@@ -65,6 +74,7 @@ def ParseArg():
     parser.add_argument("-read1", dest="read1", type=str, help="forward input fastq/fasta file")
     parser.add_argument("-read2", dest="read2", type=str, help="reverse input fastq/fasta file")
     parser.add_argument("-index", dest="index", type=str, default="", help="random index file")
+    parser.add_argument("-nbreads", dest="number_of_seqs", type=int, default=0, help="Provide number of reads in the files")
     return parser.parse_args()
 
 if __name__ == '__main__':
